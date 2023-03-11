66ffeda83054502d7689a95eca4bf6a40c634782
Prince <pmdhamecha673@gmail.com>
2023-03-11 19:12:42 +0530

    Bug fix

--- server/routes/stockRoute.js
+++ server/routes/stockRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import { assetProfle, priceChart } from "../controllers/stockCOntroller.js";
+import { assetProfle, priceChart } from "../controllers/stockController.js";
 router.route("/assetProfile").post(assetProfle);
 router.route("/priceChart").post(priceChart);
 
