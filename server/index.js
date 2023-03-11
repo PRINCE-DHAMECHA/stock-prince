a4ad064a0abea434e56375acd27d7e71cce95854
prince dhamecha <pmdhamecha673@gmail.com>
2023-03-11 19:22:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,13 +30,6 @@ app.use("/loan", authenticateUser, loanRouter);
 app.use("/account", authenticateUser, transactionRouter);
 app.use("/stock", stockRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
