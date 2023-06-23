72e928d7a98207c722770df64efee6a0d31fceb9
Prince <pmdhamecha673@gmail.com>
2023-06-23 21:04:36 +0530

    Controller refactor

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -11,15 +11,15 @@ const register = async (req, res) => {
   if (password.length <= 5) {
     throw new BadRequestError("Password Must Be at least 6 Characters Long");
   }
-  const userAlreadyExists = await User.findOne({ email });
-  if (userAlreadyExists) {
-    throw new UnAuthenticatedError("Email Already Exist");
-  }
-  const userAlreadyExists2 = await User.findOne({ name });
-  if (userAlreadyExists2) {
-    throw new UnAuthenticatedError("Username Already Taken");
-  }
   try {
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      throw new UnAuthenticatedError("Email Already Exist");
+    }
+    const userAlreadyExists2 = await User.findOne({ name });
+    if (userAlreadyExists2) {
+      throw new UnAuthenticatedError("Username Already Taken");
+    }
     const user = await User.create({ name, email, password });
     const token = user.createJWT();
     res.status(StatusCodes.CREATED).json({
@@ -35,16 +35,16 @@ const login = async (req, res) => {
   if (!email || !password) {
     throw new BadRequestError("Please provide all values");
   }
-  const user = await User.findOne({ email }).select("+password");
-  if (!user) {
-    throw new UnAuthenticatedError("Email Does Not Exist");
-  }
-
-  const isPasswordCorrect = await user.comparePassword(password);
-  if (!isPasswordCorrect) {
-    throw new UnAuthenticatedError("Invalid Credentials");
-  }
   try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      throw new UnAuthenticatedError("Email Does Not Exist");
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+      throw new UnAuthenticatedError("Invalid Credentials");
+    }
     const token = user.createJWT();
     user.password = undefined;
     res.status(StatusCodes.OK).json({ user, token });
